b4f2427704b68adea42f7147e7d1e373ceb4d24f
Annapoorani Annamalai <anuannamalai93@gmail.com>
2026-07-15 11:35:36 +0530

    updated assessment2 test

---
+++ Tests/TestHappyFlows/test_assessment2.py
@@ -0,0 +1,31 @@
+from Tests.blockads import blockads
+from pageobjects.CartPage import CartPage
+from pageobjects.LoginSignup import LoginSignup
+from pageobjects.ProductsPage import Productspage
+from pageobjects.TopMenu import TopMenu
+import allure
+from Tests.utils import get_test_data
+
+@allure.title("End to End Happy flow")
+@allure.description("Verifies that a guest user can search for products,"
+                    "add the first two 'Sleeveless' items to the cart,"
+                    "register during checkout, and successfully place an order.")
+@allure.severity(allure.severity_level.CRITICAL)
+
+def test_assessment1(page):
+
+    testdata = get_test_data()
+
+    page.route("**/*", blockads)
+    page.goto(testdata["URL"])
+
+    #object creation for required classes
+    topmenu = TopMenu(page)
+    productspage = Productspage(page)
+    cartpage = CartPage(page)
+    loginsignup = LoginSignup(page)
+
+    topmenu.verify_home_page_loaded()
+    topmenu.click_products()
+    topmenu.verify_products_page_loaded()
+
